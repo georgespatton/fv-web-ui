293d6cfb68c1a615f1240bc5d47315add28ebd09
Chad Oakenfold <web@oakenfold.ca>
2020-12-03 16:20:19 -0800

    Store init code tidy

--- frontend/app/state/store.js
+++ frontend/app/state/store.js
@@ -1,21 +1,6 @@
 import thunkMiddleware from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './reducers'
-
-// const logger = store => next => action => {
-//   console.log('dispatching', action)
-//   let result = next(action)
-//   console.log('next state', store.getState())
-//   return result
-// }
+import rootReducer from 'state/reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-export default createStore(
-  rootReducer,
-  composeEnhancers(
-    applyMiddleware(
-      // logger,
-      thunkMiddleware // lets us dispatch() functions
-    )
-  )
-)
+export default createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
