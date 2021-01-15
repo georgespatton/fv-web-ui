f5a811a1cc6b1cae25727bab7074e26123799f44
Chad Oakenfold <web@oakenfold.ca>
2021-01-15 14:17:49 -0800

    Delete V1 Test component

--- frontend/app/components/Test/index.js
+++
@@ -1,9 +0,0 @@
-import TestContainer from 'components/Test/TestContainer'
-import TestPresentation from 'components/Test/TestPresentation'
-import TestData from 'components/Test/TestData'
-
-export default {
-  Container: TestContainer,
-  Presentation: TestPresentation,
-  Data: TestData,
-}
