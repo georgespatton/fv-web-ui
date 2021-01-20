a71646f4fcb4e92631b57a2ba61aecf8328afe2a
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-01-20 14:30:58 -0800

    Update DialectHeaderMobile.js

--- frontend/app_v2/src/components/DialectHeader/DialectHeaderMobile.js
+++ frontend/app_v2/src/components/DialectHeader/DialectHeaderMobile.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import ChevronDownIcon from 'assets/svg/chevron_down.svg'
+import ChevronRightIcon from 'assets/icons/ChevronRightIcon'
+import LoginIcon from 'assets/icons/LoginIcon'
 
 /**
  * @summary DialectHeaderPresentation
@@ -14,48 +15,43 @@ import ChevronDownIcon from 'assets/svg/chevron_down.svg'
  */
 function DialectHeaderMobile({ menuData }) {
   const menus = menuData.map((menu) => (
-    <button
-      key={`${menu.title}_id`}
-      type="button"
-      onClick={() => {}}
-      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-    >
-      {menu.icon}
-      <p className="ml-3 text-base font-medium text-gray-900">{menu.title}</p>
-      {!Array.isArray(menu.itemsData) || !menu.itemsData.length ? null : <img src={ChevronDownIcon} alt="Show more" />}
-    </button>
+    <div key={`${menu.title}_id`}>
+      <button
+        type="button"
+        onClick={() => {}}
+        className="w-full m-3 p-1 text-fv-blue flex items-center hover:bg-gray-50 focus:outline-none focus:ring-0"
+      >
+        {menu.icon}
+        <p className="ml-3 text-fv-blue font-medium hover:text-fv-blue-dark">{menu.title}</p>
+        {!Array.isArray(menu.itemsData) || !menu.itemsData.length ? null : (
+          <ChevronRightIcon styling={'absolute right-3 fill-current h-12 w-8 text-lg'} />
+        )}
+      </button>
+    </div>
   ))
   return (
     <div className="inset-x-0 transition transform origin-top-right md:hidden ">
-      <div className="shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-        <div className="pt-5 pb-6 px-5">
-          <div className="mt-6">
-            <nav className="grid gap-y-8">{menus}</nav>
-          </div>
-        </div>
-        <div className="py-6 px-5 space-y-6">
-          <div>
-            {/* <a
-              href="/register?requestedUrl=/register"
-              className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-fv-orange  hover:bg-fv-orange-dark"
-            >
-              Register
-            </a>
-            <p className="mt-6 text-center text-base font-medium text-gray-500">
-              Existing customer?
-              <a href="/nuxeo/logout?requestedUrl=login.jsp" className="text-indigo-600 hover:text-indigo-500">
-                Sign in
-              </a>
-            </p> */}
-            <a
+      <div className="shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-200">
+        <nav className="grid divide-y-2 divide-gray-200">
+          {menus}
+          <div key="SignIn_id">
+            <button
               href="/nuxeo/logout?requestedUrl=login.jsp"
-              className="whitespace-nowrap text-xl font-medium text-black hover:text-gray-500"
+              type="button"
+              onClick={() => {}}
+              className="w-full m-3 p-1 text-fv-blue flex items-center hover:bg-gray-50 focus:outline-none"
             >
-              Sign in
-            </a>
+              <LoginIcon styling={'fill-current h-12 w-8 text-lg'} />
+              <p className="ml-3 text-fv-blue font-medium hover:text-fv-blue-dark">Sign in</p>
+            </button>
+          </div>
+        </nav>
+        <div className="py-6 px-5 space-y-6">
+          <div className="w-full flex items-center">
             <a
               href="/register?requestedUrl=/register"
-              className="mx-4 whitespace-nowrap inline-flex items-center justify-center border border-transparent rounded-full py-1 px-3 shadow-sm text-xl font-medium text-white bg-fv-orange hover:bg-fv-orange-dark"
+              //className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-fv-orange  hover:bg-fv-orange-dark"
+              className="w-32 h-12 mx-4 whitespace-nowrap inline-flex items-center justify-center border border-transparent rounded-full py-3 px-6 shadow-sm text-lg text-white bg-fv-orange hover:bg-fv-orange-dark"
             >
               Register
             </a>
